c4bcfad95a16603176b93de9d8d68133b95125bb
neaxi <neaxi@users.noreply.github.com>
2021-11-30 18:48:23 +0100

    async redesign - coro 3 - CLI print + removed locking

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ class Guesstimator:
         self.sd_buffer = []
 
         #
-        self.measure_count = 0
+        self.meas_count = 0
         self.ads_buffer = CNFG.ADS_ARRAY
         self.ads = {
             "volt_adj": [0] * CNFG.CHNLS,  # empty list, 4 elements
@@ -51,17 +51,21 @@ class Guesstimator:
             for channel in range(CNFG.CHNLS):
                 # read data from the sensor
                 self.ads_buffer = self.hw["ads"].read(channel1=channel)
-                # apply third polynomial conversion and write into history buffer
-                voltage = CNFG.ADS_CORRECTIONS[channel](self.ads_buffer)
-                self._adc_add_hist(channel, voltage)  # ads["avg_feed"] - not shared
 
-                async with uasyncio.Lock():
-                    # log raw values
-                    self.ads["raw"][channel] = self.ads_buffer
-                    # add manual adjust
-                    self.ads["volt_adj"][channel] = voltage + self.v_corr
-                    # published averaged values
-                    self.volt_avg[channel] = self.adc_avg_hist(channel)
+                # log raw values
+                self.ads["raw"][channel] = self.ads_buffer
+
+                # apply third polynomial conversion and
+                voltage = CNFG.ADS_CORRECTIONS[channel](self.ads_buffer)
+
+                # add manual adjust
+                self.ads["volt_adj"][channel] = voltage + self.v_corr
+
+                # write into history buffer
+                self._adc_add_hist(channel, voltage)
+
+                # published averaged values
+                self.volt_avg[channel] = self.adc_avg_hist(channel)
             await uasyncio.sleep(CNFG.T_ADS_MEAS)
 
     def _adc_add_hist(self, channel, voltage):
@@ -86,9 +90,8 @@ class Guesstimator:
     async def coro_data_prep(self):
         """safely access avg data, create a copy and add it to SD queue"""
         while True:
-            async with uasyncio.Lock():
-                tmp = list(self.volt_avg)  # instantiate a new object !!
-            self.sd_buffer.append(tmp)
+            if self.state == "meas":
+                self.sd_buffer.append(self.volt_avg)
             await uasyncio.sleep(CNFG.T_CLI_PRINT_MEAS)
 
     """
@@ -97,30 +100,58 @@ class Guesstimator:
 
     async def coro_update_lcd_voltages(self):
         # self.hw["lcd"].show_cursor()
-        # self.hw["lcd"].clear()
         while True:
-            async with uasyncio.Lock():
-                voltages = list(self.volt_avg)
             for row in range(0, CNFG.CHNLS):
-                voltage = voltages[row]
+                voltage = self.volt_avg[row]
                 msg = f"{row + 1}: {voltage:05.2f} V | "
                 self.hw["lcd"].move_to(0, row)
                 self.hw["lcd"].putstr(msg)
             await uasyncio.sleep(CNFG.T_LCD_REFRESH)
 
+    """
+    CORO 4 - print CLI values
+    """
+
+    async def coro_update_cli_values(self):
+        while True:
+            if self.state == "meas":
+                out = ["######"]
+            else:
+                out = []
+            out += [f"{self.meas_count}"]
+            out += [f"{v:05.2f}" for v in self.volt_avg]
+            out += [str(self.v_corr)]
+
+            print(CNFG.CSV_SPLIT.join(out))
+            if self.state == "setup":
+                await uasyncio.sleep(CNFG.T_CLI_PRINT_SETUP)
+            else:
+                await uasyncio.sleep(CNFG.T_CLI_PRINT_MEAS)
+
     def start(self):
         """
-        corutines
+        coroutines
         1 - measure ADC, parse values, write last 5
         2 - prepare data in queue for LCD, CLI and SD
         3 - update LCD values
+        4 - CLI value printing
         """
         print("Starting parallel execution.")
         loop = uasyncio.get_event_loop(runq_len=40, waitq_len=40)
         loop.create_task(self.coro_ads_measure())
         loop.create_task(self.coro_data_prep())
         loop.create_task(self.coro_update_lcd_voltages())
+        loop.create_task(self.coro_update_cli_values())
+        loop.create_task(debug())
         loop.run_forever()
+
+
+async def debug():
+    import gc
+
+    while True:
+        print(f"Free memory: {gc.mem_free()}")
+        await uasyncio.sleep(1)
 
 
 VM = Guesstimator()
